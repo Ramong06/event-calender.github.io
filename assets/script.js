ca5590452dc47a1137547bd98332730a2acb72bc
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-05-08 23:48:02 -0500

    added colors and hour responses to text blocks

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,29 @@
-var currentTime = moment().hours()
-//psuedo code
-//write a function that tirgets .time-block
+var currentTime = moment().hours();
+console.log(currentTime);
+
+//write a function that targets .time-block
 $('.time-block').each(function() {
-    var blockHour = $(this).attr('id')
-    console.log(blockHour);
+    var blockHour = $(this).attr('id');
+    if (currentTime > blockHour) { 
+        $('.description').removeClass('future');
+        $('.description').removeClass('present');
+        $('.description').addClass('past');
+    }
+    else if (currentTime = blockHour) {
+        $('.description').removeClass('future');
+        $('.description').removeClass('past');
+        $('.description').addClass('present');
+    }
+    else {
+        $('.description').removeClass('past');
+        $('.description').removeClass('present');
+        $('.description').addClass('future');
+    }
     // and is going to compare currentTime and id of the time block 
     //if currentime > id of the time block => .addClass('pasr) else if current = id of the block .removeClass('past) .addClass('present')
     // else rm past rm present addd future
-    
-    
 })
 
-//event button on save button
 //when save button is clicked text area will be saved to local storage
 $(".saveBtn").click(function () {
  var timeBlock = $(this).parent().attr("id")
@@ -21,5 +33,12 @@ $(".saveBtn").click(function () {
 //Render local storage
 $('#9 .description').val(localStorage.getItem('9'));
 $("#10 .description").val(localStorage.getItem("10"));
-//Add a date on the top of the page
-$("#currentDay").text(moment().format("dddd, MMMM Do"))
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+//date on the top of the page
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
