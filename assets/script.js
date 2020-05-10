6b4c4975adb300b96d293833d8e4134c6b4eae41
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-05-09 21:50:25 -0500

    final push before turning in

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@ var currentTime = moment().hours();
 
 //write a function that targets .time-block
 $('.time-block').each(function() {
-    var blockHour = $(this).attr('id');
+    var blockHour = parseInt($(this).attr('id'));
+    console.log(currentTime > blockHour);
     if (currentTime > blockHour) { 
         $('.description').removeClass('future');
         $('.description').removeClass('present');
@@ -31,10 +32,10 @@ $('#9 .description').val(localStorage.getItem('9'));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
 $("#12 .description").val(localStorage.getItem("12"));
-$("#1 .description").val(localStorage.getItem("1"));
-$("#2 .description").val(localStorage.getItem("2"));
-$("#3 .description").val(localStorage.getItem("3"));
-$("#4 .description").val(localStorage.getItem("4"));
-$("#5 .description").val(localStorage.getItem("5"));
+$("#1 .description").val(localStorage.getItem("13"));
+$("#2 .description").val(localStorage.getItem("14"));
+$("#3 .description").val(localStorage.getItem("15"));
+$("#4 .description").val(localStorage.getItem("16"));
+$("#5 .description").val(localStorage.getItem("17"));
 //date on the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
