84645682074bb9919175c6c8110331794364f1c4
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-05-11 14:21:19 -0500

    final version

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This homework assignment was assigned to me by The University of Texas at University of Houston Coding Bootcamp.  
 The purpose of this assignment is for me utilize JQuery to have an event calender that:
-1. Saves event styped into the event boxes to the local storage. 
+1. Saves events typed into the event boxes to the local storage. 
 2. Shows the events that have passed, are currently active and will be active in the future.
 3. Use colors to highlight events past, present and future. 
--- assets/script.js
+++ assets/script.js
@@ -3,39 +3,38 @@ var currentTime = moment().hours();
 //write a function that targets .time-block
 $('.time-block').each(function() {
     var blockHour = parseInt($(this).attr('id'));
-    console.log(currentTime > blockHour);
-    if (currentTime > blockHour) { 
-        $('.description').removeClass('future');
-        $('.description').removeClass('present');
-        $('.description').addClass('past');
+    if (blockHour < currentTime) { 
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass('past');
     }
     else if (currentTime === blockHour) {
-        $('.description').removeClass('future');
-        $('.description').removeClass('past');
-        $('.description').addClass('present');
+        $(this).removeClass('future');
+        $(this).removeClass('past');
+        $(this).addClass('present');
     }
     else {
-        $('.description').removeClass('past');
-        $('.description').removeClass('present');
-        $('.description').addClass('future');
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
     }
-})
+});
 
 //when save button is clicked text area will be saved to local storage
 $(".saveBtn").click(function () {
- var timeBlock = $(this).parent().attr("id")
- var valBlock = $(this).siblings(".description").val()
- localStorage.setItem(timeBlock, valBlock)
+ var timeBlock = $(this).parent().attr('id');
+ var valBlock = $(this).siblings(".description").val();
+ localStorage.setItem(timeBlock, valBlock);
 })
 //Render local storage
 $('#9 .description').val(localStorage.getItem('9'));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
 $("#12 .description").val(localStorage.getItem("12"));
-$("#1 .description").val(localStorage.getItem("13"));
-$("#2 .description").val(localStorage.getItem("14"));
-$("#3 .description").val(localStorage.getItem("15"));
-$("#4 .description").val(localStorage.getItem("16"));
-$("#5 .description").val(localStorage.getItem("17"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 //date on the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
